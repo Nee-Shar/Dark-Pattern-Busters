383964c2b0d73bfde8672dee19c68043b3174770
Neeraj Sharma <neeraj716sharma@gmail.com>
2024-01-08 22:00:56 +0530

    New dataset.tsv file added

--- Identification.py
+++ Identification.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 #First we check different count of types of Pattern Cateogry
 
-df=pd.read_csv('dark-patterns-v2.csv')
+df=pd.read_csv('dataset.tsv',delimiter='\t',encoding='utf-8')
 
-df=df[pd.notnull(df['Pattern String'])]
+df=df[pd.notnull(df['text'])]
 
 # Count different types of pattern categories and number of strings in each category
 pattern_counts = df['Pattern Category'].value_counts()
