2ca19b958ce68b5670599206bb2c77178bc5bda0
Neeraj Sharma <neeraj716sharma@gmail.com>
2024-01-19 19:53:20 +0530

    Final Undefined Fix

--- extension/content.js
+++ extension/content.js
@@ -3,7 +3,7 @@ function detectDarkPatterns() {
 
   // Extract text content from each span element, remove empty spaces, and merge multiple spaces
   const spanTexts = Array.from(spanElements).map((span) =>
-    span.innerText.replace(/\s+/g, " ").trim()
+    span.innerText.replace(/\s+/g, " ").replace(/\n/g, "").trim()
   );
 
   console.log("All Spans:", spanTexts);
@@ -36,9 +36,50 @@ function detectDarkPatterns() {
     .then((response) => response.json())
     .then((data) => {
       console.log("Predicted Categories:", data.predicted_categories);
-
+      const predictedCategories = data.predicted_categories;
       // Assuming you have a function to handle the highlighting based on the predicted categories
-      handleHighlighting(data.predicted_categories, darkPatternVariables);
+      //handleHighlighting(data.predicted_categories, darkPatternVariables);
+      for (let i = 0; i < spanTexts.length; i++) {
+        // console.log(spanTexts[i], " -> ", predictedCategories[i], i);
+        // Check if spanTexts[i] is defined and not empty
+        if (!spanTexts[i] || spanTexts[i].length === 0) {
+          continue;
+        }
+
+        if (spanTexts[i].innerText === "/" || spanTexts[i].length < 5) {
+          continue;
+        }
+        if (predictedCategories[i] === "undefined") console.log("undefined");
+        if (predictedCategories[i] !== "Not Dark Pattern") {
+          // spanElements[i].style.border = "2px solid red";
+          spanElements[i].style.backgroundColor = "aqua";
+          console.log(spanTexts[i], " -> ", predictedCategories[i]);
+        }
+
+        if (predictedCategories[i] === "Scarcity")
+          darkPatternVariables.scarcity++;
+
+        if (predictedCategories[i] === "Forced Action")
+          darkPatternVariables.forced++;
+
+        if (predictedCategories[i] === "Urgency")
+          darkPatternVariables.urgency++;
+
+        if (predictedCategories[i] === "Social Proof")
+          darkPatternVariables.proof++;
+
+        if (predictedCategories[i] === "Sneaking")
+          darkPatternVariables.sneaking++;
+
+        if (predictedCategories[i] === "Obstruction")
+          darkPatternVariables.obstruction++;
+
+        if (predictedCategories[i] === "Misdirection")
+          darkPatternVariables.misdirection++;
+
+        if (predictedCategories[i] === "Subscription trap")
+          darkPatternVariables.subtrap++;
+      }
 
       // Send a message to the extension background script with the variables
       // chrome.runtime.sendMessage({ darkPatternVariables });
@@ -61,11 +102,14 @@ function detectDarkPatterns() {
 
 function handleHighlighting(predictedCategories, darkPatternVariables) {
   const spanElements = document.querySelectorAll("span");
+
+  // Extract text content from each span element, remove empty spaces, and merge multiple spaces
   const spanTexts = Array.from(spanElements).map((span) =>
     span.innerText.replace(/\s+/g, " ").replace(/\n/g, "").trim()
   );
 
   for (let i = 0; i < spanTexts.length; i++) {
+    console.log(spanTexts[i], " -> ", predictedCategories[i], i);
     // Check if spanTexts[i] is defined and not empty
     if (!spanTexts[i] || spanTexts[i].length === 0) {
       continue;
@@ -74,7 +118,7 @@ function handleHighlighting(predictedCategories, darkPatternVariables) {
     if (spanTexts[i].innerText === "/" || spanTexts[i].length < 5) {
       continue;
     }
-    if (predictedCategories[i] == "undefined") console.log("undefined");
+    if (predictedCategories[i] === "undefined") console.log("undefined");
     if (predictedCategories[i] !== "Not Dark Pattern") {
       // spanElements[i].style.border = "2px solid red";
       spanElements[i].style.backgroundColor = "aqua";
